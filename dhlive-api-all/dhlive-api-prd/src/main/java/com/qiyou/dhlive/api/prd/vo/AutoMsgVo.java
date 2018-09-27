e9fc628d7d2e85a51bc072885a1bf3a08b84040e
18851029897 <fish.shen@iCloud.com>
2018-09-27 13:23:05 +0800

    代码提交

---
+++ dhlive-api-all/dhlive-api-prd/src/main/java/com/qiyou/dhlive/api/prd/vo/AutoMsgVo.java
@@ -0,0 +1,37 @@
+package com.qiyou.dhlive.api.prd.vo;
+
+/**
+ * Created by fish on 2018/9/27.
+ */
+public class AutoMsgVo {
+
+    private String content;
+
+    private Integer level;
+
+    private String name;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
