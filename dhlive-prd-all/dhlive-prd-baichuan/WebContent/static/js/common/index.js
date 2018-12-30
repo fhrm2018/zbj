915a6fa5ba842f84a8d94acddb542a28bf804b46
fhrm <fhrm@DESKTOP-J89UJ34>
2018-12-30 22:29:00 +0800

    66666666666666

--- dhlive-prd-all/dhlive-prd-baichuan/WebContent/static/js/common/index.js
+++ dhlive-prd-all/dhlive-prd-baichuan/WebContent/static/js/common/index.js
@@ -458,7 +458,7 @@ $(function () {
             // $(".headBtns .downUrlBtn").css('width', "10.5%");
             $(".logo").addClass("logo02");
 
-            $(".serviceList").attr('class', "serviceList clearfix posRel w1400");
+            // $(".serviceList").attr('class', "serviceList clearfix posRel w1400");
             $(".rq_ewm2").show();
             $(".rq_ewm").hide();
             $(".popularGuest h3").hide();
@@ -466,7 +466,7 @@ $(function () {
             // none(num1);
         } else if (screenWidth <= 1700) {
             // none(3);
-            $(".serviceList").attr('class', "serviceList clearfix posRel w1700");
+            // $(".serviceList").attr('class', "serviceList clearfix posRel w1700");
             $(".rq_ewm2").show();
             $(".rq_ewm").hide();
             $(".popularGuest h3").hide();
@@ -477,17 +477,17 @@ $(function () {
             $(".downUrlBtn").css('width', "auto");
             $(".logo").removeClass("logo02");
 
-            $(".serviceList").attr('class', "serviceList clearfix posRel w1800");
+            // $(".serviceList").attr('class', "serviceList clearfix posRel w1800");
             $(".rq_ewm").show();
             $(".rq_ewm2").hide();
         }
 
         var winHeight = win.height();
-        var serviceListHeight = $('.contentRight .serviceList').outerHeight(true);
+        // var serviceListHeight = $('.contentRight .serviceList').outerHeight(true);
         var messageBoxHeight = $('.contentRight .messageBox').outerHeight(true);
         var tishiHeight = $('.tishi').outerHeight(true);
-        var videoHeight = winHeight - 40 - 150 - 60 - 46;        // 40 footer; 150 banner margin; 60 header margin; 46 viewTime;
-        var contentRightMsg = winHeight - 60 - serviceListHeight - messageBoxHeight - 40 - 30 - tishiHeight;
+        var videoHeight = winHeight - 40 - 150 - 60 - 46 - 80;        // 40 footer; 150 banner margin; 60 header margin; 46 viewTime;
+        var contentRightMsg = winHeight - 60 - messageBoxHeight - 40 - 30 - tishiHeight;
         var newLeft = winHeight - 60 - 40;
 
         if (winHeight < 800) {
