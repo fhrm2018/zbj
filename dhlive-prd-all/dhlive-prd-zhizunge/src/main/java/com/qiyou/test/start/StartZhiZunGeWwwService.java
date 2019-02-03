96691262e0b7a5305c11d49da60c99f9cee1828d
admin <admin@DESKTOP-5PBCT3P>
2019-02-03 10:24:20 +0800

    1111

--- dhlive-prd-all/dhlive-prd-zhizunge/src/main/java/com/qiyou/test/start/StartZhiZunGeWwwService.java
+++ dhlive-prd-all/dhlive-prd-zhizunge/src/main/java/com/qiyou/test/start/StartZhiZunGeWwwService.java
@@ -5,7 +5,7 @@ import com.yaozhong.framework.base.start.StartWebApp;
 /**
  * Created by liuyijun on 2016/5/31.
  */
-public class StartZhongYingWwwService {
+public class StartZhiZunGeWwwService {
 
     public static void main(String[] args) throws Exception {
         StartWebApp.main(args);
